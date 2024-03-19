d51891415a34acb8fb73cfb68ffcf912ad0df4e2
Tiffany <tiffanyman19@gmail.com>
2024-03-19 16:17:47 -0400

    Complete

--- server/app.py
+++ server/app.py
@@ -14,66 +14,63 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages', methods=['GET','POST'])
+@app.route('/messages', methods= ['GET','POST'])
 def messages():
+    messages = Message.query.order_by(Message.created_at).all()
     
     if request.method == 'GET':
-        messages = Message.query.order_by(Message.created_at.asc()).all()
         
-        messages_list = [message.to_dict() for message in messages]
-        return jsonify(messages_list), 200
+        message_list = []
+        for message in messages:
+            message_dict = message.to_dict()
+            message_list.append(message_dict)
+        response = make_response(jsonify(message_list),200)       
+        return response
     
     elif request.method == 'POST':
-        data=request.json
-        username = data.get("username")
-        body = data.get("body")
+        data=request.get_json()
         
-        if username is None or body is None:
-            return jsonify({"error": "Missing required fields"}), 400
-        
-        new_message = Message(username=username,body=body)
-        
+        new_message = Message(
+            body=data["body"],
+            username=data["username"],
+            
+        )
         db.session.add(new_message)
         db.session.commit()
         
-        return jsonify(new_message.to_dict()), 201
+        new_message_dict =new_message.to_dict()
         
-@app.route('/messages/<int:id>', methods=['GET','PATCH','DELETE'])
+        response = make_response(jsonify(new_message_dict), 201)
+        
+        return response
+    
+@app.route('/messages/<int:id>', methods= ['PATCH','DELETE'])
 def messages_by_id(id):
-    
-    if request.method == 'GET':
-        message = Message.query.filter_by(id=id).first()
+    message=Message.query.filter_by(id = id).first()
+    if request.method == 'PATCH':
+        data=request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
         
-        if not message:
-            return jsonify({"error": "Message not found"}), 404
-
-        return jsonify(message.to_dict()), 200
-    
-    elif request.method == 'PATCH':
-        message = Message.query.filter_by(id=id).first()
-
-        if not message:
-            return jsonify({"error": "Message not found"}), 404
-        
-        data = request.json
-        for attr, value in data.items():
-            setattr(message, attr, value)
-        
+        db.session.add(message)
         db.session.commit()
         
-        return jsonify(message.to_dict()), 200
+        message_dict= message.to_dict()
+        response = make_response(jsonify(message_dict))
+        
+        return response
     
     elif request.method == 'DELETE':
-        message = Message.query.filter_by(id=id).first()
-
-        if not message:
-            return jsonify({"error": "Message not found"}), 404
-
         db.session.delete(message)
         db.session.commit()
         
-        return jsonify({"deleted":"Message deleted succesfully"})            
-    
+        response_body = {
+            'delete_successfully': True,
+            'message':'Baked good deleted'
+        }
+        return(jsonify(response_body),200)
+    return "LLL"
+
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
     
